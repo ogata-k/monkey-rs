7875b45914da7f38b9fcbafafc65a2fedaff7492
k-ogata <visor.ogata@gmail.com>
2019-05-30 12:27:48 +0900

    テストケースを拡張

--- src/token.rs
+++ src/token.rs
@@ -34,28 +34,28 @@ pub struct Token {
     literal: String,
 }
 
-impl Token{
+impl Token {
     /// 初期化関数
-    pub fn new(token_type: TokenType, literal: &str) -> Self{
-        return Token{
+    pub fn new(token_type: TokenType, literal: &str) -> Self {
+        return Token {
             token_type,
-            literal: literal.to_string()
+            literal: literal.to_string(),
         };
     }
 }
 
 #[cfg(test)]
 mod test {
+    use crate::lexer::Lexer;
     use crate::token::Token;
     use crate::token::TokenType;
-    use crate::lexer::Lexer;
 
     #[test]
-    fn test_no_line(){
+    fn test_no_line() {
         let input = "";
         let tests = [Token::new(TokenType::EOF, "")];
         let mut lexer = Lexer::new(input);
-        for (i, tt) in tests.iter().enumerate(){
+        for (i, tt) in tests.iter().enumerate() {
             let tok = lexer.next_token();
 
             assert_eq!(tok.token_type, tt.token_type);
@@ -65,7 +65,18 @@ mod test {
 
     #[test]
     fn test_next_token() {
-        let input = "=+(){},;";
+        let input =
+        "
+        +(){},;
+        let five = 5;
+        let ten = 10;
+
+        let add = fn(x, y) {
+            x + y;
+        };
+
+        let result = add(five, ten);
+        ";
 
         let tests = [
             Token::new(TokenType::ASSIGN, "="),
@@ -76,12 +87,48 @@ mod test {
             Token::new(TokenType::RBRACE, "}"),
             Token::new(TokenType::COMMA, ","),
             Token::new(TokenType::SEMICOLON, ";"),
+            Token::new(TokenType::LET, "let"),
+            Token::new(TokenType::IDENT, "five"),
+            Token::new(TokenType::ASSIGN, "="),
+            Token::new(TokenType::INT, "5"),
+            Token::new(TokenType::SEMICOLON, ";"),
+            Token::new(TokenType::LET, "let"),
+            Token::new(TokenType::IDENT, "ten"),
+            Token::new(TokenType::ASSIGN, "="),
+            Token::new(TokenType::INT, "10"),
+            Token::new(TokenType::SEMICOLON, ";"),
+            Token::new(TokenType::LET, "let"),
+            Token::new(TokenType::IDENT, "add"),
+            Token::new(TokenType::ASSIGN, "="),
+            Token::new(TokenType::FUNCTION, "fn"),
+            Token::new(TokenType::LPAREN, "("),
+            Token::new(TokenType::IDENT, "x"),
+            Token::new(TokenType::COMMA, ","),
+            Token::new(TokenType::IDENT, "y"),
+            Token::new(TokenType::RPAREN, ")"),
+            Token::new(TokenType::LBRACE, "{"),
+            Token::new(TokenType::IDENT, "x"),
+            Token::new(TokenType::PLUS,  "+"),
+            Token::new(TokenType::IDENT, "y"),
+            Token::new(TokenType::SEMICOLON, ";"),
+            Token::new(TokenType::RBRACE, "}"),
+            Token::new(TokenType::SEMICOLON, ";"),
+            Token::new(TokenType::LET, "let"),
+            Token::new(TokenType::IDENT, "result"),
+            Token::new(TokenType::ASSIGN, "="),
+            Token::new(TokenType::IDENT, "add"),
+            Token::new(TokenType::LPAREN, "("),
+            Token::new(TokenType::IDENT, "five"),
+            Token::new(TokenType::COMMA, ","),
+            Token::new(TokenType::IDENT, "ten"),
+            Token::new(TokenType::RPAREN, ")"),
+            Token::new(TokenType::SEMICOLON, ";"),
             Token::new(TokenType::EOF, "")
         ];
 
         let mut lexer = Lexer::new(input);
 
-        for (i, tt) in tests.iter().enumerate(){
+        for (i, tt) in tests.iter().enumerate() {
             let tok = lexer.next_token();
 
             assert_eq!(tok.token_type, tt.token_type);
