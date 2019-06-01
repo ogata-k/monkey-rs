453f5a03a7ea57420376d58b6af99375f97eb5d5
ogata-k <ogtkzk712@gmail.com>
2019-06-01 18:01:22 +0900

    モジュールに関するdocコメントが書かれていなかったので追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
+//! Monkey言語の処理系に関するモジュールです。
+//! Monkey言語に関してはREADMEを参照してください。
+
+/// 字句解析が返しうるトークン列に関するモジュール
 pub mod token;
+
+/// 字句解析用モジュール
 pub mod lexer;
-pub mod repl;
+
+/// パーサー(構文解析器)が返しうる木構造のノードに関するモジュール
 pub mod ast;
+
+/// REPLを扱うためのモジュール
+pub mod repl;
