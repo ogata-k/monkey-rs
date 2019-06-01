98a4f93a29db9ce55876541a7f1f0625ef763c89
ogata-k <ogtkzk712@gmail.com>
2019-06-01 15:59:19 +0900

    対話的に実行できるようにmain関数を実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io::{Read, Write, stdin, stdout};
+use monkey_rs::repl::start;
+
 fn main() {
-    println!("Hello, world!");
+    let r = stdin();
+    let mut w = stdout();
+
+    writeln!(w, "Hello! This is the Monkey programming language written by Rust.");
+    writeln!(w, "Feel free to type in commands.");
+    writeln!(w, "If you want to finish REPL, input Ctrl+D and push Enter.");
+    start(r, w);
 }
--- src/repl.rs
+++ src/repl.rs
@@ -1,20 +1,26 @@
-use std::io::{Read, Write, BufReader, BufWriter, BufRead};
+use std::io::{Read, Write, BufReader, BufWriter, BufRead, LineWriter};
 use crate::lexer::Lexer;
 use crate::token::{Token, TokenType};
 
 /// 入力促進メッセージ
 const PROMPT: &str = ">> ";
+/// REPL終了用の入力記号
+const FINISH_KEY: &str = "\u{4}";
 
 /// 入力を受けて改行区切りのトークン列に変換する関数
-pub fn start(mut reader: impl Read, writer: impl Write) {
+pub fn start(reader: impl Read, writer: impl Write) {
     let mut r = BufReader::new(reader);
-    let mut w = BufWriter::new(writer);
+    let mut w = LineWriter::new(writer);
 
-    loop {
-        write!(w, "{}",  PROMPT).expect("panic! writer error!");
+     loop {
+        write!(w, "{}",  PROMPT);
+        w.flush();
         let mut line = "".to_string();
         let res = r.read_line(&mut line);
         if res.is_err(){
+            break;
+        }
+        if line.trim() == FINISH_KEY{
             break;
         }
         let mut lexer = Lexer::new(&line);
@@ -23,7 +29,7 @@ pub fn start(mut reader: impl Read, writer: impl Write) {
             if tok == Token::new(TokenType::EOF, "") {
                 break;
             }
-            write!(w, "{:?}\n", tok).expect("panic! writer error!");
+            write!(w, "{:?}\n", tok);
         }
     }
 }
