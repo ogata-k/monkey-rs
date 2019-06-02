0aab0f8e1c47d3d06602183f734dff54b747f51a
ogata-k <ogtkzk712@gmail.com>
2019-06-02 13:50:40 +0900

    パーサーのテストを実行できる形にした

--- src/ast.rs
+++ src/ast.rs
@@ -10,11 +10,12 @@ pub trait Node {
 pub enum Statement {
     // ここにStatementに関する構造体を定義していく
     /// let文用のノード
+    /// <token> <name> = <value>;
+    /// つまり、let <name> = <value>;
     LetStatement {
-        token: Token,
-        // nameはExpression::Identifier限定なのでつくるときに判定を
-        name: Box<Expression>,
-        value: Box<Expression>,
+        token: Token,  // Token::LET
+        name: Box<Expression>,  // 束縛対象の変数名、Expression::Identifierのみ
+        value: Box<Expression>,  // 束縛する対象
     }
 }
 
@@ -34,8 +35,8 @@ pub enum Expression {
     // ここにExpressionに関する構造体を定義していく
     /// 識別子を表すノード
     Identifier {
-        token: Token,
-        value: String,
+        token: Token, // Token::Ident
+        value: String,  // 識別子が保持する値
     }
 }
 
@@ -49,12 +50,29 @@ impl Node for Expression {
     }
 }
 
+impl Expression {
+    /// 保持する値のゲッター
+    pub fn get_value(&self) -> String{
+        match self {
+            Expression::Identifier{token, value} => {
+                value.to_string()
+            }
+        }
+    }
+}
+
 /// Monkeyプログラムをあらわす構造体
 pub struct Program {
     statements: Vec<Statement>,
 }
 
 impl Program {
+    /// programのstatementsのゲッター
+    pub fn get_statements(&self) -> &Vec<Statement> {
+        return &self.statements;
+}
+
+/// ルートノードであるprogramノードのリテラルを返す
     pub fn token_literal(&self) -> String {
         if self.statements.len() > 0 {
             return self.statements[0].token_literal();
--- src/parser.rs
+++ src/parser.rs
@@ -31,6 +31,7 @@ impl Parser {
         };
     }
 
+
     /// 保持している字句解析器を使って一文字読む関数
     pub fn next_token(&mut self) {
         std::mem::swap(&mut self.current_token, &mut self.peek_token);
@@ -38,7 +39,62 @@ impl Parser {
     }
 
     /// 字句解析器の結果を元にMonkeyプログラムを表す解釈木を生成する関数
-    pub fn parse_program(&mut self) -> Program {
+    pub fn parse_program(&mut self) -> Option<Program> {
+        // 簡易的にOption型にしているがResultを返すように修正してもよい
         unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod test{
+    use crate::lexer::Lexer;
+    use crate::parser::Parser;
+    use crate::ast::*;
+    use crate::token::*;
+
+    /// let文の構文解析用のテスト
+    #[test]
+    fn test_let_statements() {
+        let input = "
+            let x = 5;
+            let y = 10;
+            let foobar = 838383;
+        ";
+
+        let mut lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        let program_opt = parser.parse_program();
+        if program_opt.is_none() {
+            assert!(false,"let文のパースに失敗しました。");
+        }
+        let program = program_opt.unwrap();
+        if program.get_statements().len() != 3 {
+            assert!(false, "let文の個数が不適切です。");
+        }
+
+        let tests = ["x", "y", "foobar"];
+
+        for (i, test) in tests.iter().enumerate() {
+            let stmt = &(program.get_statements()[i]);
+            test_let_statement(stmt, test);
+        }
+    }
+
+    // 束縛される値は後でやるとして、束縛時の変数名をテストする関数
+    fn test_let_statement(stmt: &Statement, test: &str){
+        match stmt {
+            Statement::LetStatement{token, name, value} => {
+                // トークンのletで始まってるか確認
+                assert_eq!(token.get_literal(), "test");
+                // 束縛変数名の確認
+                assert_eq!(name.get_value(), test);
+                // TODO 束縛された値の確認
+                // assert_eq!(value.get_value(), "");
+
+
+            },
+            _ => {assert!(false, "let文ではありません。");}
+        }
     }
 }
