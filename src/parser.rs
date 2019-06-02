a5c04fb95979dcf744711b87b4a5463c52e327ac
ogata-k <ogtkzk712@gmail.com>
2019-06-02 16:25:44 +0900

    パースエラーもどきを実装したが、参考書では一応すべてのチェックをするのに対してこの実装では最初の失敗で解析が終了する。

--- src/parser.rs
+++ src/parser.rs
@@ -6,8 +6,12 @@ use crate::ast::Program;
 /// パーサー(構文解析器)
 pub struct Parser {
     lexer: Lexer,
+    // 入力を元にトークンを返すための字句解析器
     current_token: Token,
+    // 現在読んでいるトークン
     peek_token: Token,
+    // 一つ先のトークン
+    errors: Vec<String>,  // パースシテ失敗したときのエラー文の集まり
 }
 
 impl std::fmt::Debug for Parser {
@@ -29,9 +33,14 @@ impl Parser {
             lexer,
             current_token: first,
             peek_token: second,
+            errors: Vec::new(),
         };
     }
 
+    /// パースエラーを返す関数
+    pub fn get_errors(&self) -> Vec<String> {
+        return self.errors.clone();
+    }
 
     /// 保持している字句解析器を使って一文字読む関数
     pub fn next_token(&mut self) {
@@ -39,13 +48,23 @@ impl Parser {
         self.peek_token = self.lexer.next_token();
     }
 
+    ///  異常なトークンを検出した場合のエラー
+    fn make_illegal_error(&mut self){
+        let msg = "異常なトークンを検出しました。".to_string();
+        self.errors.push(msg);
+    }
+
+    /// 文のパースに失敗した場合のエラー
+    fn make_statement_parse_error(&mut self) {
+        let msg = "文をパースできませんでした。".to_string();
+        self.errors.push(msg);
+    }
+
     /// 字句解析器の結果を元にMonkeyプログラムを表す解釈木を生成する関数
     pub fn parse_program(&mut self) -> Option<Program> {
-        // 簡易的にOption型にしているがResultを返すように修正してもよい
         let mut program = Program::new();
         let eof: Token = Token::new(TokenType::EOF, "");
 
-        // 今まはOption型のため簡易的に例外処理をpanicで済ましている。
         loop {
             // 正常終了
             if self.current_token == eof {
@@ -54,14 +73,16 @@ impl Parser {
 
             // 異常終了
             if self.current_token.is_illegal_token() {
-                panic!("異常なトークンを検出しました。");
+                self.make_illegal_error();
+                break;
             }
 
             let stmt_opt = self.parse_statement();
 
             // 異常終了(後日式にも対応したら変更する必要がある)
             if stmt_opt.is_none() {
-                panic!("文をパースできませんでした。");
+                self.make_statement_parse_error();
+                break;
             }
             let stmt = stmt_opt.unwrap();
             program.statements.push(stmt);
@@ -76,14 +97,20 @@ impl Parser {
     }
 
     /// 先のトークンの型を確認する関数
-    fn peek_token_is(&self, token_type: TokenType) -> bool{
+    fn peek_token_is(&self, token_type: TokenType) -> bool {
         return self.peek_token.get_token_type() == token_type;
+    }
+
+    /// 先読み時に発生したエラー用をフォーマットを使って生成して追加する。
+    fn make_peek_error(&mut self, expect_type: TokenType) {
+        let msg = format!("トークン型{:?}を期待して読み込みましたが、実際に読み込んだトークン型は{:?}でした。", expect_type, self.current_token.get_token_type());
+        self.errors.push(msg);
     }
 
     /// 文用のパーサー
     pub fn parse_statement(&mut self) -> Option<Statement> {
         if self.current_token.is_let_token() {
-            return self.parse_let_statement()
+            return self.parse_let_statement();
         } else {
             return None;
         }
@@ -91,10 +118,11 @@ impl Parser {
 
     /// 次の型を読んで予期している型かどうかを判定する関数
     fn expect_peek(&mut self, token_type: TokenType) -> bool {
-        if self.peek_token_is(token_type) {
+        if self.peek_token_is(token_type.clone()) {
             self.next_token();
             return true;
         } else {
+            self.make_peek_error(token_type);
             return false;
         }
     }
@@ -126,10 +154,10 @@ impl Parser {
             self.next_token();
         }
         let let_statement = Statement::LetStatement {
-                token: Token::new(TokenType::LET, "let"),
-                name: Box::new(ident),
-                value: Box::new(ident_stub),
-            };
+            token: Token::new(TokenType::LET, "let"),
+            name: Box::new(ident),
+            value: Box::new(ident_stub),
+        };
         return Some(let_statement);
     }
 }
@@ -140,6 +168,7 @@ mod test {
     use crate::parser::Parser;
     use crate::ast::*;
     use crate::token::*;
+    use std::io::stderr;
 
     /// let文の構文解析用のテスト
     #[test]
@@ -148,6 +177,9 @@ mod test {
             let x = 5;
             let y = 10;
             let foobar = 838383;
+            let z 4;
+            let = 10;
+            let 838383;
         ";
 
         let mut lexer = Lexer::new(input);
@@ -155,11 +187,13 @@ mod test {
 
         let program_opt = parser.parse_program();
         if program_opt.is_none() {
+            check_parser_errors(&parser);
             assert!(false, "let文のパースに失敗しました。");
         }
         let program = program_opt.unwrap();
         let statements = &program.statements;
         if statements.len() != 3 {
+            check_parser_errors(&parser);
             assert!(false, "let文の個数が不適切です。");
         }
 
@@ -169,6 +203,7 @@ mod test {
             let stmt = &statements[i];
             test_let_statement(stmt, test);
         }
+        check_parser_errors(&parser);
     }
 
     // 束縛される値は後でやるとして、束縛時の変数名をテストする関数
@@ -183,6 +218,19 @@ mod test {
                 // assert_eq!(value.get_value(), "");
             }
             _ => { assert!(false, "let文ではありません。"); }
+        }
+    }
+
+    fn check_parser_errors(parser: &Parser) {
+        use std::io::Write;
+        let errors = parser.get_errors();
+        if errors.len() == 0 {
+            return;
+        }
+        let mut e_writer = std::io::stderr();
+        writeln!(e_writer, "パースエラーが{}件発生しました。", errors.len());
+        for error in errors {
+            writeln!(e_writer, "{}", error);
         }
     }
 }
