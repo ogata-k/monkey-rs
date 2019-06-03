cb9b7de6ac6ca04cd5b687a292978400a1ee6347
ogata-k <ogtkzk712@gmail.com>
2019-06-03 21:46:09 +0900

    ノードを文字列に変換できるようにメソッドを追加

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,9 @@
+use std::fmt::Write;
+
 use crate::token::Token;
 
 /// ノード
-pub trait Node {
+pub trait Node: ToString {
     fn token_literal(&self) -> String;
 }
 
@@ -15,7 +17,8 @@ pub enum Statement {
     LetStatement {
         // Token::LET
         token: Token,
-        name: Box<Expression>,  // 束縛対象の変数名、Expression::Identifierのみ
+        name: Box<Expression>,
+        // 束縛対象の変数名、Expression::Identifierのみ
         value: Box<Expression>, // 束縛する対象
     },
     /// return文用のノード
@@ -25,6 +28,32 @@ pub enum Statement {
         // Token::Return
         token: Token,
         return_value: Box<Expression>, // 戻り値
+    },
+}
+
+impl ToString for Statement {
+    fn to_string(&self) -> String {
+        let mut s = "".to_string();
+        match self {
+            Statement::LetStatement { token, name, value } => {
+                write!(s, "{}", token.get_literal() + " ").unwrap();
+                write!(s, "{}", name.to_string()).unwrap();
+                let v = value.to_string();
+                if v != "".to_string() {
+                    write!(s, " {} {}", "=", &v).unwrap();
+                }
+                write!(s, "{}", ";").unwrap();
+            }
+            Statement::ReturnStatement { token, return_value } => {
+                write!(s, "{}", token.get_literal() + " ").unwrap();
+                let v = return_value.to_string();
+                if v != "".to_string() {
+                    write!(s, " {} {}", " =", &v).unwrap();
+                }
+                write!(s, "{}", ";").unwrap();
+            },
+        }
+        return s;
     }
 }
 
@@ -41,6 +70,7 @@ impl Node for Statement {
 #[derive(Debug, PartialEq)]
 pub enum Expression {
     // ここにExpressionに関する構造体を定義していく
+    NonValue,
     /// 識別子を表すノード
     Identifier {
         token: Token,
@@ -49,10 +79,26 @@ pub enum Expression {
     },
 }
 
+impl ToString for Expression {
+    fn to_string(&self) -> String {
+        let mut s = "".to_string();
+        match self {
+            Expression::NonValue => {
+                write!(s, "{}", "").unwrap();
+            },
+            Expression::Identifier { token: _, value } => {
+                write!(s, "{}", value).unwrap();
+            }
+        }
+        return s;
+    }
+}
+
 impl Node for Expression {
     fn token_literal(&self) -> String {
         match self {
             Expression::Identifier { token, value: _ } => token.get_literal(),
+            Expression::NonValue => { "".to_string() }
         }
     }
 }
@@ -62,6 +108,38 @@ impl Expression {
     pub fn get_value(&self) -> String {
         match self {
             Expression::Identifier { token: _, value } => value.to_string(),
+            Expression::NonValue => "".to_string(),
+        }
+    }
+}
+
+/// 式文用のノード
+#[derive(Debug, PartialEq)]
+pub enum ExpressionStatement {
+    ExpressionStatement {
+        token: Token,
+        // 式の最初のトークン
+        expression: Expression,
+    }
+}
+
+impl ToString for ExpressionStatement {
+    fn to_string(&self) -> String {
+        match self {
+            ExpressionStatement::ExpressionStatement { token: _, expression } => {
+                if *expression != Expression::NonValue {
+                    return expression.to_string();
+                }
+                return "".to_string();
+            }
+        }
+    }
+}
+
+impl Node for ExpressionStatement {
+    fn token_literal(&self) -> String {
+        match self {
+            ExpressionStatement::ExpressionStatement { token, expression: _ } => token.get_literal(),
         }
     }
 }
@@ -70,6 +148,16 @@ impl Expression {
 #[derive(Debug)]
 pub struct Program {
     pub statements: Vec<Statement>,
+}
+
+impl ToString for Program {
+    fn to_string(&self) -> String {
+        let mut s = "".to_string();
+        for stmt in self.statements.iter() {
+            write!(s, "{}", stmt.to_string()).unwrap();
+        }
+        return s;
+    }
 }
 
 impl Program {
