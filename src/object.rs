22aac317b1a16513ae226d158b583f34cc1616bc
k-ogata <visor.ogata@gmail.com>
2019-07-18 12:31:51 +0900

    整数型オブジェクトを追加

--- src/object.rs
+++ src/object.rs
@@ -1,11 +1,35 @@
+// 定数
+pub const INTEGER_OBJECT: &str = "INTEGER";
+
 /// オブジェクトシステム上で管理するための型情報
 #[derive(Debug, Eq, PartialEq, Clone, Hash)]
-pub enum ObjectType {
-    StringObject(String),
+pub struct ObjectType {
+    object_type: String,
+}
+
+impl ObjectType {
+    pub fn integer_object_type() -> Self {
+        ObjectType {
+            object_type: INTEGER_OBJECT.to_string(),
+        }
+    }
 }
 
 /// オブジェクトシステム上で扱うオブジェクト情報
-trait Object {
-    fn get_type(&self) -> ObjectType;
-    fn inspect(&self) -> String;
+#[derive(Debug, PartialEq, Clone, Hash)]
+pub enum Object {
+    Integer { value: i64 },
+}
+
+impl Object {
+    pub fn get_type(&self) -> ObjectType {
+        match self {
+            Object::Integer { value: _ } => ObjectType::integer_object_type(),
+        }
+    }
+    pub fn inspect(&self) -> String {
+        match self {
+            Object::Integer { value } => format!("{}", value),
+        }
+    }
 }
