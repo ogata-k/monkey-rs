bd7ed5622e2a8606879b9f8ce077ac2cb28e3c88
k-ogata <visor.ogata@gmail.com>
2019-07-18 12:40:47 +0900

    Nullオブジェクトを追加

--- src/object.rs
+++ src/object.rs
@@ -1,4 +1,5 @@
 // 定数
+pub const NULL_OBJECT: &str = "NULL";
 pub const INTEGER_OBJECT: &str = "INTEGER";
 pub const BOOLEAN_OBJECT: &str = "BOOLEAN";
 
@@ -14,9 +15,16 @@ impl ObjectType {
             object_type: INTEGER_OBJECT.to_string(),
         }
     }
+
     pub fn boolean_object_type() -> Self {
         ObjectType {
             object_type: BOOLEAN_OBJECT.to_string(),
+        }
+    }
+
+    pub fn null_object_type() -> Self {
+        ObjectType {
+            object_type: NULL_OBJECT.to_string(),
         }
     }
 }
@@ -24,6 +32,7 @@ impl ObjectType {
 /// オブジェクトシステム上で扱うオブジェクト情報
 #[derive(Debug, PartialEq, Clone, Hash)]
 pub enum Object {
+    Null,
     Integer { value: i64 },
     Boolean { value: bool },
 }
@@ -31,12 +40,14 @@ pub enum Object {
 impl Object {
     pub fn get_type(&self) -> ObjectType {
         match self {
+            Object::Null => ObjectType::null_object_type(),
             Object::Integer { value: _ } => ObjectType::integer_object_type(),
             Object::Boolean { value: _ } => ObjectType::boolean_object_type(),
         }
     }
     pub fn inspect(&self) -> String {
         match self {
+            Object::Null => "null".to_string(),
             Object::Integer { value } => format!("{}", value),
             Object::Boolean { value } => format!("{}", value),
         }
