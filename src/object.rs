6d57f17bcf173fef032d7ee5082e22b0654af018
k-ogata <visor.ogata@gmail.com>
2019-07-18 12:36:25 +0900

    真偽値オブジェクトを追加

--- src/object.rs
+++ src/object.rs
@@ -1,5 +1,6 @@
 // 定数
 pub const INTEGER_OBJECT: &str = "INTEGER";
+pub const BOOLEAN_OBJECT: &str = "BOOLEAN";
 
 /// オブジェクトシステム上で管理するための型情報
 #[derive(Debug, Eq, PartialEq, Clone, Hash)]
@@ -13,23 +14,31 @@ impl ObjectType {
             object_type: INTEGER_OBJECT.to_string(),
         }
     }
+    pub fn boolean_object_type() -> Self {
+        ObjectType {
+            object_type: BOOLEAN_OBJECT.to_string(),
+        }
+    }
 }
 
 /// オブジェクトシステム上で扱うオブジェクト情報
 #[derive(Debug, PartialEq, Clone, Hash)]
 pub enum Object {
     Integer { value: i64 },
+    Boolean { value: bool },
 }
 
 impl Object {
     pub fn get_type(&self) -> ObjectType {
         match self {
             Object::Integer { value: _ } => ObjectType::integer_object_type(),
+            Object::Boolean { value: _ } => ObjectType::boolean_object_type(),
         }
     }
     pub fn inspect(&self) -> String {
         match self {
             Object::Integer { value } => format!("{}", value),
+            Object::Boolean { value } => format!("{}", value),
         }
     }
 }
